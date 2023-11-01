3c2bd39926944d212ca6d9b19a785df64bdb5661
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-01 08:58:39 +0530

    Update Bubble Sort.cpp

--- Data Structures And Algorithm/Sorting Algorithms/Bubble Sort.cpp
+++ Data Structures And Algorithm/Sorting Algorithms/Bubble Sort.cpp
@@ -6,21 +6,14 @@ using namespace std;
 void Bubble_Sort(int arr[], int n)
 {
     int i, j;
-    bool swapped;
-    for(i=0; i<n-1; i++)
+    for(i=n-1; i>=0; i--)
     {
-        swapped=false;
-        for(j=0; j<n-i-1; j++)
+        for(j=0; j<i; j++)
         {
             if(arr[j]>arr[j+1])
             {
                 swap(arr[j], arr[j+1]);
-                swapped=true;
             }
-        }
-        if(swapped==false)
-        {
-            break;
         }
     }
 }
