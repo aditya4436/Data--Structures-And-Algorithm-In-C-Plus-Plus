3b2ec76a9e2cf5a023bcc4bcd90db405fb2fdb33
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-01 09:18:23 +0530

    Update Bubble Sort.cpp

--- Data Structures And Algorithm/Sorting Algorithms/Bubble Sort.cpp
+++ Data Structures And Algorithm/Sorting Algorithms/Bubble Sort.cpp
@@ -1,21 +1,31 @@
 // Time Complexity:- O(N^2), 'N' is the number of element.
+// Best Time Complexity:- O(N), after optimization
 // Auxiliary Space Complexity:- O(1)
 #include<bits/stdc++.h>
 #include<algorithm>
 using namespace std;
 void Bubble_Sort(int arr[], int n)
 {
-    int i, j;
+    int i, j, count=0;
+    bool swapped;
     for(i=n-1; i>=0; i--)
     {
+        swapped=false;
         for(j=0; j<i; j++)
         {
             if(arr[j]>arr[j+1])
             {
                 swap(arr[j], arr[j+1]);
+                swapped=true;
             }
         }
+        if(swapped==false)
+        {
+            break;
+        }
+        count++;
     }
+    cout<<"Loop executed "<<count<<" times to sort an array.";
 }
 void Print_Array(int arr[], int size)
 {
@@ -27,10 +37,10 @@ void Print_Array(int arr[], int size)
 }
 int main()
 {
-    int arr[]={64, 34, 25, 12, 22, 11, 90};
+    int arr[]={13, 46, 24, 52, 20, 9};
     int size=sizeof(arr)/sizeof(arr[0]);
     Bubble_Sort(arr, size);
-    cout<<"Sorted array is:\n";
+    cout<<"\nSorted array is:\n";
     Print_Array(arr, size);
     return 0;
 }
