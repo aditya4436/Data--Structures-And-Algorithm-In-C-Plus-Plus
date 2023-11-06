77b72c5a5dcd83f0170dda5e365ddb688730cbbf
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-06 14:03:30 +0530

    Add files via upload

---
+++ Data Structures And Algorithm/Sorting Algorithms/Count Sort.cpp
@@ -0,0 +1,41 @@
+// Time Complexity:- O(n+m), for best, average and worst case and 'n' and 'm'
+//                   are the size of input_array and count_array.
+// Auxiliary Space Complexity:- O(n+m), where 'n' and 'm' are the space taken
+//                              by output_array and count_array respectively.
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> Count_Sort(vector<int> &input_array)
+{
+    int n=input_array.size();
+    int m=0;
+    for(int i=0; i<n; i++)
+    {
+        m=max(m, input_array[i]);
+    }
+    vector<int> count_array(m+1, 0);
+    for(int i=0; i<n; i++)
+    {
+        count_array[input_array[i]]++;
+    }
+    for(int i=1; i<=m; i++)
+    {
+        count_array[i]+=count_array[i-1];
+    }
+    vector<int> output_array(n);
+    for(int i=n-1; i>=0; i--)
+    {
+        output_array[count_array[input_array[i]]-1]=input_array[i];
+        count_array[input_array[i]]--;
+    }
+    return output_array;
+}
+int main()
+{
+    vector<int> input_array={4, 3, 12, 1, 5, 5, 3, 9};
+    vector<int> output_array=Count_Sort(input_array);
+    for(int i=0; i<input_array.size(); i++)
+    {
+        cout<<output_array[i]<<"  ";
+    }
+    return 0;
+}
