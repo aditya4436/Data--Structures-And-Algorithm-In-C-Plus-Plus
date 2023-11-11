fc40b4184c6ec01a61ced3ca6f4794c81a739692
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-11 10:17:19 +0530

    Update Largest And Second Largest Element.cpp

--- Data Structures And Algorithm/Array/Solved Problems On Array/Largest And Second Largest Element.cpp
+++ Data Structures And Algorithm/Array/Solved Problems On Array/Largest And Second Largest Element.cpp
@@ -1,7 +1,7 @@
-// Time Complexity:- For both SecondLargestElement() and LargestElement() is same i.e., O(n).
+// Time Complexity:- O(n).
 #include<bits/stdc++.h>
 using namespace std;
-int SecondLargestElement(vector<int> &arr, int n)
+int LargestAndSecondLargestElement(vector<int> &arr, int n)
 {
     int largest=arr[0];
     int slargest=INT_MIN;
@@ -17,25 +17,13 @@ int SecondLargestElement(vector<int> &arr, int n)
             slargest=arr[i];
         }
     }
+    cout<<"The largest element is:\n"<<largest<<endl;
     return slargest;
-}
-int LargestElement(vector<int> &arr, int n)
-{
-    int largest=arr[0];
-    for(int i=0; i<n; i++)
-    {
-        if(arr[i]>largest)
-        {
-            largest=arr[i];
-        }
-    }
-    return largest;
 }
 int main()
 {
     vector<int> arr={10, 25, -12, -6, 20, 20 };
     int n=arr.size();
-    cout<<"The first largest element:\n"<<LargestElement(arr, n);
-    cout<<"\nThe second largest element:\n"<<SecondLargestElement(arr, n);
+    cout<<"The second largest element:\n"<<LargestAndSecondLargestElement(arr, n);
     return 0;
 }
