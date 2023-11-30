f3db00d1f4a51330a129d3323cf8f5f3d239c7f0
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-11-30 15:05:12 +0530

    Update Binary Search.cpp

--- Data Structures And Algorithm/Searching Algorithms/Binary Search.cpp
+++ Data Structures And Algorithm/Searching Algorithms/Binary Search.cpp
@@ -2,6 +2,30 @@
 // Auxiliary Space Complexity:- O(1).
 #include<iostream>
 using namespace std;
+// int RecursiveBinarySearch(vector<int> &arr, int low, int high, int target)
+// {
+//     if(low>high)
+//     {
+//         return -1;
+//     }
+//     int mid=(low+high)/2;
+//     if(arr[mid]==target)
+//     {
+//         return mid;
+//     }
+//     else if(target>arr[mid])
+//     {
+//         return RecursiveBinarySearch(arr, mid+1, high, target);
+//     }
+//     return RecursiveBinarySearch(arr, low, mid-1, target);
+// }
+// int search(vector<int> &nums, int target)
+// {
+//     return RecursiveBinarySearch(nums, 0, nums.size()-1, target);
+// }
+
+// Time Complexity:- O(log N), where n is the number of elements in an array.
+// Auxiliary Space Complexity:- O(1).
 int Binary_Search(int arr[], int low, int high, int target)
 {
     while(low<=high)
