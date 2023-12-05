144714eaafd28bc09bed291b5c23d0ae64305876
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-12-05 07:48:00 +0530

    Update Square Root Of N.cpp

--- Data Structures And Algorithm/Searching Algorithms/Solved Problems On Binary Search/Square Root Of N.cpp
+++ Data Structures And Algorithm/Searching Algorithms/Solved Problems On Binary Search/Square Root Of N.cpp
@@ -6,16 +6,16 @@ int SquareRoot(vector<int> &arr, int n){
     int size=arr.size(),ans=-1;
     int low=0, high=size-1;
     while(low<=high){
-        int mid=(low+high)/2;
-        if(arr[mid]*arr[mid]<=n){
-            ans=arr[mid];
-            low=mid+1;
+        long long mid=(low+high)/2;
+        long long value=mid*mid;
+        if (value <= n) {
+          low = mid + 1;
         }
-        else{
-            high=mid-1;
+         else {
+          high = mid - 1;
         }
     }
-    return ans;
+    return high;
 }
 int main()
 {
