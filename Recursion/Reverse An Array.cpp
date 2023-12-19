1c0f07f2916b80d46da68a3b40e82f966629dc8e
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-12-19 17:13:30 +0530

    Update Reverse An Array.cpp

--- Recursion/Reverse An Array.cpp
+++ Recursion/Reverse An Array.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 #include<algorithm>
 using namespace std;
+// Time Complexity:- O(n)
+// Space Complexity:- O(n), this space is used internally by the computer
+//                    to store the output of previously generated result
+//                    untill the base condition is not satisfied.  
 int Reverse_An_Array(int arr[], int n)
 {
     if(n==0||n==1)
@@ -9,7 +13,19 @@ int Reverse_An_Array(int arr[], int n)
     }
     cout<<arr[n-1]<<"  ";
     return (Reverse_An_Array(arr, n-1));
-}
+} 
+// void PrintReverse(int arr[], int l, int r){
+//     if(l>r){
+//         return;
+//     }
+//     swap(arr[l], arr[r]);
+//     PrintReverse(arr, l+1, r-1);
+// }
+// void PrintArray(int arr[], int n){
+//     for(int i=0; i<n; i++){
+//         cout<<arr[i]<<"  ";
+//     }
+// }
 int main()
 {
     int n, arr[5];
