6dd552c96253e42d0c55425862062f8038e8438c
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-12-19 16:38:21 +0530

    Update Sum Using Recursion.cpp

--- Recursion/Sum Using Recursion.cpp
+++ Recursion/Sum Using Recursion.cpp
@@ -11,6 +11,14 @@ void PrintSum(int i, int sum){
     }
     PrintSum(i-1, sum+i);
 }
+
+// OTHER METHOD
+// int PrintSum(int n){
+//     if(n==0){
+//         return 0;
+//     }
+//     return (n+PrintSum(n-1));
+// }
 int main(){
     int n=3, sum=0;
     PrintSum(n, sum);
