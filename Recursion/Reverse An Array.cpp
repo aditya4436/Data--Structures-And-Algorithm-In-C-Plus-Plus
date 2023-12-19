d08ae294e5bc642c34259a68a8abd698ea70afe4
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2023-12-19 19:16:49 +0530

    Update Reverse An Array.cpp

--- Recursion/Reverse An Array.cpp
+++ Recursion/Reverse An Array.cpp
@@ -1,40 +1,44 @@
-#include<iostream>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;
 // Time Complexity:- O(n)
 // Space Complexity:- O(n), this space is used internally by the computer
 //                    to store the output of previously generated result
-//                    untill the base condition is not satisfied.  
-int Reverse_An_Array(int arr[], int n)
-{
-    if(n==0||n==1)
-    {
-        return n;
+//                    untill the base condition is not satisfied.   
+void ReverseArray(int arr[], int n){
+    if(n==0){
+        return;
     }
-    cout<<arr[n-1]<<"  ";
-    return (Reverse_An_Array(arr, n-1));
-} 
-// void PrintReverse(int arr[], int l, int r){
+    cout<<arr[n-1]<<" ";
+    ReverseArray(arr, n-1);
+}
+
+// void ReverseArray(int arr[], int l, int r){
 //     if(l>=r){
 //         return;
 //     }
 //     swap(arr[l], arr[r]);
-//     PrintReverse(arr, l+1, r-1);
+//     ReverseArray(arr, l+1, r-1);
 // }
-// void PrintArray(int arr[], int n){
-//     for(int i=0; i<n; i++){
-//         cout<<arr[i]<<"  ";
+
+// void ReverseArray(int arr[], int n, int i){
+//     if(i>=n/2){
+//         return;
 //     }
+//     swap(arr[i], arr[n-i-1]);
+//     ReverseArray(arr, n, i+1);
 // }
-int main()
-{
-    int n, arr[5];
-    n=sizeof(arr)/sizeof(arr[0]);
-    for(int i=0;i<5;i++)
-    {
-        cout<<"Enter the array element:\n";
-        cin>>arr[i];
+void PrintArray(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<"  ";
     }
-    cout<<"Reversed Array is:\n"<<Reverse_An_Array(arr, n);
+}
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=5, i=0;
+    int l=0, r=n-1;
+    // ReverseArray(arr, n, i);
+    // ReverseArray(arr, l, r);
+    ReverseArray(arr, n);
+    // PrintArray(arr, n);
     return 0;
 }
