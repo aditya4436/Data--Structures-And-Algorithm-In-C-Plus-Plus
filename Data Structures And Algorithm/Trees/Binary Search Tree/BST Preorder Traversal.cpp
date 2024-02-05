e0a50553a5faa671f6daf363342f932740bebf6b
Aditya Kumar Shah <143188965+aditya4436@users.noreply.github.com>
2024-02-05 15:46:01 +0530

    Update BST Preorder Traversal.cpp

--- Data Structures And Algorithm/Trees/Binary Search Tree/BST Preorder Traversal.cpp
+++ Data Structures And Algorithm/Trees/Binary Search Tree/BST Preorder Traversal.cpp
@@ -42,6 +42,29 @@ void Preorder_Traversal(Node *root)
         Preorder_Traversal(root->right);
     }
 }
+
+// Iterative Preorder Traversal.
+// vector<int> preorderTraversal(TreeNode* root)
+// {
+//     vector<int> ans;
+//     if(root==NULL){
+//         return ans;
+//     }
+//     stack<TreeNode*> st;
+//     st.push(root);
+//     while(!st.empty()){
+//         TreeNode *node=st.top();
+//         st.pop();
+//         ans.push_back(node->val);
+//         if(node->right){
+//             st.push(node->right);
+//         }
+//         if(node->left){
+//             st.push(node->left);
+//         }
+//     }
+//     return ans;
+// }
 int main()
 {
     struct Node *root=NULL;
